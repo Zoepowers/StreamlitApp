3b44b8b387ae4a2c1f8d4911e487318e560480b2
Zoepowers <129912858+Zoepowers@users.noreply.github.com>
2023-06-12 16:56:19 -0400

    Added Description in App

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-uploaded_file = st.sidebar.file_uploader("Choose a file")
 
+st.write("""Streamlit Application
+
+This project is a Streamlit app that allows you to upload a CSV file, analyze its contents, and perform various statistical operations on the dataset. After uploading a CSV file, the app displays relevant statistics such as the number of rows, columns, and counts of categorical, numerical, and boolean variables. You can select a single column from the dataset using an input widget. If the selected column is numerical, a table presents the five-number summary, and a customized distribution plot is created using matplotlib and seaborn. For categorical columns, a table shows the proportions of each category level, and a customized bar plot visualizes the data.""")
+
+
+st.write("Instructions:")
+st.write("1> Clone the repository")
+st.write("2> Install the packages listed in requirements.txt")
+st.write("3> Run the app using streamlit run csv_analyzer.py")
+st.write("4> Upload CSV 5> Change the different paramters in the webapp")
+
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
